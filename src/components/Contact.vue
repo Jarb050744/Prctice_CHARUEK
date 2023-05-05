ccc3965009aeb29c6664939649f7ffb68c31c543
Charurk Thongsawaeng <121765838+Jarb050744@users.noreply.github.com>
2023-05-05 18:00:48 +0700

    Initial commit

---
+++ src/components/Contact.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <dir>
+    <br />
+    <br />
+    <img src="../assets/jj.jpg" class="my-3" contain height="200" />
+    <br />
+    <br />
+    <br />
+
+    <p>
+      โซเชียลมีเดีย :
+      <button
+        style="
+          background-color: rgb(11, 27, 250);
+          font-family: verdana;
+          color: darkgray;
+        "
+        onclick="location.href='https://www.facebook.com/profile.php?id=100043717815600'"
+        type="button"
+      >
+        facebook
+      </button>
+
+      <b class="oo"
+        >Email :
+        <a class="oo" href="mailto:oppo357774@gmail.com"
+          >Charuek Thongsahwang</a
+        ></b
+      >
+      <br />
+    </p>
+  </dir>
+</template>
+
---
+++ src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <br />
+    <br />
+    <img src="../assets/jj.jpg" class="my-3" contain height="200" />
+    <br />
+    <br />
+    <h3 class="align1:left">
+      ชื่อ : นาย จารึก ทองแสวง ชื่อเล่น : จ๊าบ <br />
+      เกิดเมื่อวันที่ : 5 กรกฎาคม 2544 <br />
+      เบอร์โทรศัพท์ : 0960623544 <br />
+    </h3>
+  </div>
+</template>
+
+<script>
+</script>
